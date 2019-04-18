7a5ea9e43a34b2a454bf310a336d1f7bdb13a0da
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-18 06:11:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-  
+  planeteer_calls.collect 
 end
 
 def long_planeteer_calls# code an argument here
