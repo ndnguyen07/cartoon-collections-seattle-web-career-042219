49e5d737278b046b6b097ca8286dda523a220ea4
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-18 06:22:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def long_planeteer_calls(call_long)
   call_long.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese()
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
