adf9a8336440e3b81cb9c32af76296412ee34a44
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-18 06:10:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,9 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet(calls)
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+  
 end
 
 def long_planeteer_calls# code an argument here
