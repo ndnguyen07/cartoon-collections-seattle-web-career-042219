c17e7b75dca261bdb81f9dd6322f69b4aa808828
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-18 06:18:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(call_long)\
+  
 end
 
 def find_the_cheese# code an argument here
